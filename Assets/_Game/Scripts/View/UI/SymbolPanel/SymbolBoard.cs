d2705230368e7ca0502fe8c0122e305d67989df5
Han <147114637+hanhercales@users.noreply.github.com>
2026-02-01 03:32:45 +0700

    Edit UI

--- Assets/_Game/Scripts/View/UI/SymbolPanel/SymbolBoard.cs
+++ Assets/_Game/Scripts/View/UI/SymbolPanel/SymbolBoard.cs
@@ -1,21 +1,29 @@
 using System;
 using System.Collections.Generic;
+using _Game.Scripts.Controllers.Machines;
 using _Game.Scripts.Core.Data;
 using UnityEngine;
 
 public class SymbolBoard : MonoBehaviour
 {
+    private float totalWeight = 0f;
+
     private void OnEnable()
-    {
-        SetUpUI();
-    }
-    public void SetUpUI()
     {
         for (int i = 0; i < this.transform.childCount; i++)
         {
             GameObject child = this.transform.GetChild(i).gameObject;
-            
-            child.GetComponent<SymbolSlot>().TakeInformation();
+            totalWeight += child.GetComponent<SymbolSlot>().symbolData.currentWeight;
+        }
+        
+        SetUpUI();
+    }
+    private void SetUpUI()
+    {
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            GameObject child = this.transform.GetChild(i).gameObject;
+            child.GetComponent<SymbolSlot>().TakeInformation(totalWeight);
         }
     }
 }
--- Assets/_Game/Scripts/View/UI/SymbolPanel/SymbolSlot.cs
+++ Assets/_Game/Scripts/View/UI/SymbolPanel/SymbolSlot.cs
@@ -1,21 +1,25 @@
+using System.Globalization;
+using _Game.Scripts.Controllers.Machines;
 using _Game.Scripts.Core.Data;
+using _Game.Scripts.Core.Logic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SymbolSlot : MonoBehaviour
 {
-    public Image icon;
-    public TextMeshProUGUI symbolName;
-    public TextMeshProUGUI currentWeight;
-    public TextMeshProUGUI value;
+    [SerializeField] private Image icon;
+    [SerializeField] private TextMeshProUGUI currentWeight;
+    [SerializeField] private TextMeshProUGUI value;
+    
     public SymbolData symbolData;
 
-    public void TakeInformation()
+    public void TakeInformation(float totalWeight)
     {
+        float weight = (float) symbolData.currentWeight / totalWeight * 100f;
+        
         icon.sprite = symbolData.icon;
-        symbolName.text = symbolData.idName;
-        currentWeight.text = symbolData.currentWeight.ToString();
+        currentWeight.text = weight.ToString("F1", CultureInfo.InvariantCulture) + '%';
         value.text = symbolData.currentValue.ToString();
     }
 }
